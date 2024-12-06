d4750c8d2b2ce5e37a2f8ce2e2e3f64f5927df37
mxmmsv <oikioikioikiabrams@gmail.com>
2024-12-06 22:05:34 +0300

    feat: add convertToRomanNumerals task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -135,8 +135,32 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let number = num;
+  let result = '';
+
+  while (number >= 10) {
+    result += 'X';
+    number -= 10;
+  }
+  if (number >= 9) {
+    result += 'IX';
+    number -= 9;
+  }
+  if (number >= 5) {
+    result += 'V';
+    number -= 5;
+  }
+  if (number >= 4) {
+    result += 'IV';
+    number -= 4;
+  }
+  while (number >= 1) {
+    result += 'I';
+    number -= 1;
+  }
+
+  return result;
 }
 
 /**
